ada33a9290ab925c35e13678dd654fa37468acc2
jazznov <jazznov@Live.com>
2017-09-04 16:34:33 -0700

    Add error handling to currency formatter

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,17 @@
 import isNumber from 'lodash/isNumber';
 
-export const formatToDollars = (number) => {
-	return number.toLocaleString('en-US', {
-		style: 'currency',
+export const formatToDollars = (value = console.error('Value undefined passed to formatToDollars')) =>{
+
+	const format = value => value.toLocaleString('en-US', {
+		style:    'currency',
 		currency: 'USD',
 	});
+
+	switch(isNumber(value)){
+		case true:
+			return format(value);
+			break;
+		case false:
+			return format(parseInt(value));
+	}
 };
