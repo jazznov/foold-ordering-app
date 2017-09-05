5cc7adce7567e9043ba4767bb9cbeca75ba4d362
jazznov <jazznov@Live.com>
2017-09-05 00:45:17 -0700

    Fix Quantity Controller props, change to number input instead of range

--- src/components/QuantityController.js
+++ src/components/QuantityController.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const QuantityController = (props) => {
+const QuantityController = ({id, unitCost, quantity, onQuantityChange}) => {
+
 	return (
-		<input name={props.id}  value={props.itemQuantity}  type='range' step='1' min='0' />
+		<input name={id} value={quantity} onChange={(e) => onQuantityChange({id, unitCost, quantity: e.target.value})}  type='number' step='1' min='0' />
 	)
 };
 
 export default QuantityController;
 
 QuantityController.propTypes = {
-	handleQuantityChange: PropTypes.func,
-	value: PropTypes.number
+	onQuantityChange: PropTypes.func.isRequired,
+	value: PropTypes.number,
+	quantity:PropTypes.number
 };
 
 
